d3f944cd316ea08679f7e4ab4f1a5271dfbf0840
PeachRapit1022 <ia.rapit22@gmail.com>
2023-01-03 15:55:54 +0900

    Step2 トグルリスト化

--- src/items/step2Table.tsx
+++ src/items/step2Table.tsx
@@ -16,32 +16,49 @@ type Items = {
 
 const Step2Table = (props:Items) => {
 
+    const [view, setView] = React.useState<JSX.Element>(<></>)
+    const [viewSatus, setViewStatus] = React.useState<boolean>(true)
+
+    const table = (
+        <div className="shadow-sm overflow-hidden my-2">
+            <table className="border-collapse table-auto w-full text-sm">
+                <Step2ListHead/>
+                <tbody className="bg-white dark:bg-slate-800">
+                    {props.result.map((item: Item) => {
+                        return (
+                            <>
+                                <Step2ListItem 
+                                category = {item.category}
+                                sum = {item.sum}
+                                result = {item.result}
+                                />
+                            </>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+
+
+    const handleClickTable = () =>{
+        setViewStatus(!viewSatus)
+        if (viewSatus) {
+            setView(table)
+        } else {
+            setView(<></>)
+        }
+    }
+
     return (
         <div className='my-12'>
-            <Step2TableTitle
-            rule = {props.rule}
-            result = {props.result}
-            />
-            <div className="shadow-sm overflow-hidden my-2">
-                <table className="border-collapse table-auto w-full text-sm">
-                    <Step2ListHead/>
-                    <tbody className="bg-white dark:bg-slate-800">
-                        {props.result.map((item: Item) => {
-                            
-                            return (
-                                <>
-                                    <Step2ListItem 
-                                    category = {item.category}
-                                    sum = {item.sum}
-                                    result = {item.result}
-                                    />
-                                </>
-                            )
-
-                        })}
-                    </tbody>
-                </table>
+            <div onClick={handleClickTable}>
+                <Step2TableTitle
+                rule = {props.rule}
+                result = {props.result}
+                />
             </div>
+            {view}
         </div>
     )
 }
